305547ad3ac2252f653ed7abc7076e0a56ab2ad1
indra purnomo <indrapurnomoa@gmail.com>
2023-03-27 14:29:28 +0700

    add id primaryKey to table playlists_songs

--- migrations/1679892539919_create-table-playlists-songs.js
+++ migrations/1679892539919_create-table-playlists-songs.js
@@ -1,43 +1,47 @@
 /* eslint-disable camelcase */
 
 exports.up = (pgm) => {
-    pgm.createTable('playlists_songs', {
-        playlistId: 'VARCHAR(30) NOT NULL',
-        songId: 'VARCHAR(25) NOT NULL',
-    });
+  pgm.createTable('playlists_songs', {
+    id: {
+      type: 'VARCHAR(30)',
+      primaryKey: true,
+    },
+    playlistId: 'VARCHAR(30) NOT NULL',
+    songId: 'VARCHAR(25) NOT NULL',
+  });
 
-    pgm.addConstraint('playlists_songs', 'fk_playlist', {
-        foreignKeys: {
-            columns: 'playlistId',
-            references: 'playlists(id)',
-            onDelete: 'cascade',
-            onUpdate: 'cascade',
-        },
-    });
+  pgm.addConstraint('playlists_songs', 'fk_playlist', {
+    foreignKeys: {
+      columns: 'playlistId',
+      references: 'playlists(id)',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    },
+  });
 
-    pgm.addConstraint('playlists_songs', 'fk_song', {
-        foreignKeys: {
-            columns: 'songId',
-            references: 'songs(id)',
-            onDelete: 'cascade',
-            onUpdate: 'cascade',
-        },
-    });
+  pgm.addConstraint('playlists_songs', 'fk_song', {
+    foreignKeys: {
+      columns: 'songId',
+      references: 'songs(id)',
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    },
+  });
 };
 
 exports.down = (pgm) => {
-    pgm.dropConstraint('playlists_songs', 'fk_playlist', {
-        ifExists: true,
-        cascade: true,
-    });
+  pgm.dropConstraint('playlists_songs', 'fk_playlist', {
+    ifExists: true,
+    cascade: true,
+  });
 
-    pgm.dropConstraint('playlists_songs', 'fk_song', {
-        ifExists: true,
-        cascade: true,
-    });
+  pgm.dropConstraint('playlists_songs', 'fk_song', {
+    ifExists: true,
+    cascade: true,
+  });
 
-    pgm.dropTable('playlists_songs', {
-        ifExists: true,
-        cascade: true,
-    });
+  pgm.dropTable('playlists_songs', {
+    ifExists: true,
+    cascade: true,
+  });
 };
